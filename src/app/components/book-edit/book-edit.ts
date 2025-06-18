946469298b2603f703be07fa27a945bc7b4f6793
nayla <nayla.kouider@edu.unifi.it>
2025-06-18 12:58:24 +0200

    Added book-edit component

---
+++ src/app/components/book-edit/book-edit.ts
@@ -0,0 +1,76 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { MatDialogRef } from '@angular/material/dialog';
+import {BookService} from '../../service/book-service';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {BookModel} from '../../model/book-model';
+
+@Component({
+  selector: 'app-book-edit',
+  imports: [
+    ReactiveFormsModule
+  ],
+  standalone:true,
+  templateUrl: './book-edit.html',
+  styleUrl: './book-edit.css'
+})
+export class BookEdit implements OnInit {
+
+  bookForm?:FormGroup;
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data:{ id:string},
+    private bookService: BookService,
+    private fb: FormBuilder,
+    public dialogRef: MatDialogRef<BookEdit>) {
+
+
+  }
+  ngOnInit() {
+    this.initForm();
+    this.bookService.getBook(this.data.id).subscribe(book =>{
+      this.form.patchValue({
+        title: book.title,
+        author: book.author,
+        genre: book.genre,
+        publishedYear: book.publishedYear,
+        pages: book.pages,
+        isbn: book.isbn,
+        available:book.available,
+        score: book.score
+      })
+    } );
+  }
+  get form() {
+    return this.bookForm as FormGroup;
+  }
+  closeModal(){
+    this.dialogRef.close();
+  }
+  saveBook(){
+    if(this.form.invalid){
+      return;
+    }
+    let book: BookModel ={
+      id: this.data.id,
+      ...this.form.value
+    }
+    this.bookService.updateBook(book).subscribe(()=>{
+      this.dialogRef.close("updated")
+    })
+  }
+  private initForm(){
+    this.bookForm= this.fb.group({
+      title: ['', Validators.required],
+      author: ['', Validators.required],
+      genre:['', Validators.required],
+      publishedYear:['', Validators.required],
+      pages: ['', Validators.required],
+      isbn: ['', Validators.required],
+      available: ['', Validators.required],
+      score :['', Validators.required],
+    })
+  }
+
+
+}
+
